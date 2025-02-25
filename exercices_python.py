04103c1822d05e4e78d54d1175e25d2281643208
CharlesRLG <charlesrabeclegloahec@gmail.com>
2025-02-25 22:17:32 +0100

    Exercices 1 à 3

---
+++ exercices_python.py
@@ -0,0 +1,25 @@
+# Exercice 1: écrire 3 variable (a,b,c) avec comme valeurs respective 1,"France", 36.2 et les affichés dans la console
+
+a = 1
+b = "France"
+c = 36,2
+
+print (a, b, c)
+
+# Exercice 2: écrire une variable ch avec comme valeur "salut" changé sa valeur avec "ça va ?" puis affiché la variable dans la console.
+
+ch = "salut"
+ch ="ça va ?"
+
+print (ch)
+
+# Exercice 3: déclarer deux variables x et y, avec 3 et 8,5, convertissez les en chaine de charactère puis affiché les avec leurs type dans la console.
+
+x = 3
+y = 8,5
+
+x = str(x)
+y = str(y)
+
+print(x, type(x))
+print(y, type(y))
