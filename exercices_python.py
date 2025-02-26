1c205d0d5617f1d46c7e6cd00c3ebabf49145b89
CharlesRLG <charlesrabeclegloahec@gmail.com>
2025-02-26 11:19:22 +0100

    Ajout des exercice 4 à 6

--- exercices_python.py
+++ exercices_python.py
@@ -1,25 +1,55 @@
 # Exercice 1: écrire 3 variable (a,b,c) avec comme valeurs respective 1,"France", 36.2 et les affichés dans la console
 
-a = 1
-b = "France"
-c = 36,2
+#a = 1
+#b = "France"
+#c = 36,2
 
-print (a, b, c)
+#print (a, b, c)
 
 # Exercice 2: écrire une variable ch avec comme valeur "salut" changé sa valeur avec "ça va ?" puis affiché la variable dans la console.
 
-ch = "salut"
-ch ="ça va ?"
+#ch = "salut"
+#ch ="ça va ?"
 
-print (ch)
+#print (ch)
 
 # Exercice 3: déclarer deux variables x et y, avec 3 et 8,5, convertissez les en chaine de charactère puis affiché les avec leurs type dans la console.
 
-x = 3
-y = 8,5
+#x = 3
+#y = 8,5
 
-x = str(x)
-y = str(y)
+#x = str(x)
+#y = str(y)
 
-print(x, type(x))
-print(y, type(y))
+#print(x, type(x))
+#print(y, type(y))
+
+# Exercice 4: Ecrire un programme qui demande le poids en Kg, le stocker dans une variable puis l'afficher.
+
+#poid = ""
+
+#poid = input ("Quel est votre poids ? ( en Kg) ")
+
+#print (" Votre poids est de " + poid + " Kg !" )
+
+# Exercice 5: Ecricre une variable Var contenant "Bonjour" si c'est une chaine de caractère affiché "chaine de caractère" sinon si c'est un entier affiché "entier".
+
+#var = "Bonjour"
+
+#if type(var) == str :
+    #print("Chaine de caractère")
+#elif type(var) == int:
+    #print("Entier")
+#else:
+    #print("Inconnu")
+
+# Exercice 6: Déclarer un chiffre positif ou négatif, si il est positif afficher Positif sinon affiché Négatif.
+
+chiffre = 0
+
+chiffre = int(input("Donnez un chiffre positif ou négatif ?"))
+
+if chiffre >= 0:
+    print("Positif")
+else :
+    print("négatif")
